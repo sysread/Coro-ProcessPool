e7bdaa911b6c0fadc450049f49b4d80255f354fc
Jeff Ober <jeffober@gmail.com>
2015-03-04 08:34:22 -0500

    Bumped version to 0.17

--- lib/Coro/ProcessPool.pm
+++ lib/Coro/ProcessPool.pm
@@ -11,7 +11,7 @@ use Coro::Channel;
 use Coro::ProcessPool::Process;
 use Coro::ProcessPool::Util qw(cpu_count);
 
-our $VERSION = '0.16_01';
+our $VERSION = '0.17';
 
 if ($^O eq 'MSWin32') {
     die 'MSWin32 is not supported';
